32f3902797c46b535848609a1cf2f093c9240563
Hayato oyanagi <oyana@oya.local>
2017-01-21 23:48:23 +0900

    add scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   validates :name, :twitter_id, presence: true
   validates :twitter_id, uniqueness: true
 
+
   scope :published, -> { where(published: true)   }
+  scope :unpublished, -> { where(published: false)   }
 end
