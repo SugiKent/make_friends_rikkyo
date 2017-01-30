7dabcb3398de07298fac93e3c97e5c4972b9fc01
SugiKent <ynmk1995@gmail.com>
2017-01-30 16:17:34 +0900

    refs #44 サークルを管理画面で追加・削除・編集できるように

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,3 +1,6 @@
 class Circle < ActiveRecord::Base
+
+  validates :name, :category, presence: true
+  validates :name, uniqueness: true
   enum category: { sport: 0, culture: 1 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 		resources :users, :except => [:show]
 		resources :faculties
 		resources :departments
+    resources :circles
   end
 
 end
