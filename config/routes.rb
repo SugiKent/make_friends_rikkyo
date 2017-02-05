3ed6736e7594f5d1734a37566c3392d6b60bab14
Hayato oyanagi <oyana@oya.local>
2017-02-05 15:20:23 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
 
 	resources :users, except: [:new, :create] do
 		collection do
+      get :search
+      get :search_form
 			get :departments_select
 			get :thanks
 		end
 	end
-	
+
 	namespace :admin do
 		resources :users, :except => [:show]
 		resources :faculties
