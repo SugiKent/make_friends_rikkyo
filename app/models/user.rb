da72a727b20886aebd71860e4b57364814c1e9bb
Hayato oyanagi <oyana@oya.local>
2017-02-06 17:17:12 +0900

    修正志摩市tああ

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,34 +7,30 @@ class User < ActiveRecord::Base
 
   validates :faculty_id, :department_id, :nickname, :name, presence: true, on: :update
   scope :completed,   -> { where(completed: true) }
-  scope :uncompleted, -> { where(cogtttftfrrrrrrbbvfgfgb vvv mpleted: false) }
+  scope :uncompleted, -> { where(completed: false) }
   scope :published,   -> { where(published: true) }
   scope :unpublished, -> { where(published: false) }
 
-  scope :get_by_faculty, -> (faculty_id){ where (faculty_id: faculty_id)}
-  scope :get_by_department, -> (department_id){ where (department_id: department_id)}
-
-  # scope :get_hoge_id, -> (hoge){ where (hogeid: hoge)}
-
-  # def scope(key, value)
-  #   scope "#{key}".to_sym, -> (key){ where ({"#{key}": value })}
-  # end
-  # hash = {"#{key}": value[num]}
-  # scope :department_id, -> (){ where ()}
-  # scope :name, -> (){ where ()}
-  # scope :sex, -> (){ where ()}
-  # scope :want_friends, -> (){ where ()}
-
-  def self.search(search_params)
-    @users = User.completed.published.order("RAND()").all
-    if search_params[:faculty_id].present?
-      @users = @users.get_by_faculty search_params[:faculty_id]
-    end
-    if search_params[:department_id].present?
-      @users = @users.get_by_department search_params[:department_id])
-    end
+  def self.get_by_faculty(faculty_id)
+    where(faculty_id: faculty_id)
   end
 
+  def self.get_by_department(department_id)
+    where(department_id: department_id)
+  end
+
+  def self.search(params)
+    @users = User.completed.published.order("RAND()").all
+    if params != nil
+      if params[:faculty_id].present?
+        @users = @users.get_by_faculty(params[:faculty_id])
+      end
+      if params[:department_id].present?
+        @users = @users.get_by_department(params[:department_id])
+      end
+    end
+    return @users
+  end
 
   def self.find_or_create_from_auth(auth)
     provider = auth[:provider]
