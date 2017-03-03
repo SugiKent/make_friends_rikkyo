84945915c5b86d94c8325fa059dac5f0e6129763
SugiKent <ynmk1995@gmail.com>
2017-03-03 21:15:24 +0900

    refs #86 topic#newのフォーム修正

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
   validates :title, :description, presence: true
   belongs_to :user
   has_many :posts
+
+  scope :without_department, -> { where.not(belong_type: "department") }
 end
